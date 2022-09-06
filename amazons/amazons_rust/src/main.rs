df2f01ae63686fa72df1e40a281486451d47b825
Lu <lu.maltsis@gmail.com>
2022-09-06 17:42:30 +0200

    added rust AI

---
+++ amazons/amazons_rust/src/main.rs
@@ -0,0 +1,7 @@
+mod game;
+
+
+// main function
+fn main() {
+    game::read_and_print_move();
+}
--- cogs/amazons.py
+++ cogs/amazons.py
@@ -3,6 +3,7 @@ import discord
 import random
 import time
 import json
+import subprocess
 from discord.ext import tasks, commands
 
 from amazons import game, agents
@@ -29,6 +30,7 @@ class AmazonsCog(commands.Cog):
 
         game_id = game_data['id']
 
+        '''
         if game_data['turn'] == game.Player.white.value:
             own = game.Piece.white_amazon
         else:
@@ -42,20 +44,29 @@ class AmazonsCog(commands.Cog):
             for column in row:
                 piece = game.Piece(column)
                 board[-1].append(piece)
-
         ai = agents.DeepTerror("DeepTerror Knife", lambda d: 1/min(d, 3), -0.2, 5, 5, 6, 2)
         move, value = ai.select_move(board, own)
-        
-        (start_y, start_x), (move_y, move_x), (shoot_y, shoot_x) = move
+
+         (start_y, start_x), (move_y, move_x), (shoot_y, shoot_x) = move
 
         comment = f'Response by AI ``{ai.name}``\nBoard State Evaluation: {value}'
         move_string = f'<@963682692732428330>|play|{game_id}|{start_x},{start_y}|{move_x},{move_y}|{shoot_x},{shoot_y}|{comment}'
+        '''
+        
+        input_string = str(game_data['turn']) + ' ' + str(game_data['board'])
+        with open('amazons/input.txt', 'w') as f:
+            f.write(input_string)
+
+        subproc = subprocess.run(['wsl', './/amazons//run_amazons_rust_build.sh'], capture_output=True)
+        output = subproc.stdout.decode('utf-8')
+        
+        move_string = f'<@963682692732428330> play {game_id} {output}'
         await msg.channel.send(move_string)
         
 
     @commands.command()
     async def amazon_start(self, ctx, opponent : str):
-        await ctx.send(f'<@963682692732428330> | start | {opponent}')
+        await ctx.send(f'<@963682692732428330> start {opponent}')
 
 
 async def setup(bot: commands.Bot):
